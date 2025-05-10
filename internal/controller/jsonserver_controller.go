7845b20d858ec38bd8ded5d45b2ecf271d7a117c
Iftekhar <11455025+chickenbeef@users.noreply.github.com>
2025-05-10 16:29:13 +0000

    feat: Add jsonConfig validator and status update logic

--- internal/controller/jsonserver_controller.go
+++ internal/controller/jsonserver_controller.go
@@ -18,7 +18,9 @@ package controller
 
 import (
 	"context"
+	"encoding/json"
 
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -52,11 +54,29 @@ type JsonServerReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.20.4/pkg/reconcile
 func (r *JsonServerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = logf.FromContext(ctx)
+	log := logf.FromContext(ctx)
+	log.Info("Reconciling JsonServer", "name", req.NamespacedName)
 
-	// TODO(user): your logic here
+	// Fetch the JsonServer instance
+	jsonServer := &examplev1.JsonServer{}
+	err := r.Get(ctx, req.NamespacedName, jsonServer)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			log.Info("JsonServer resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - retry in next cycle
+		log.Error(err, "Failed to get JsonServer")
+		return ctrl.Result{}, err
+	}
 
-	return ctrl.Result{}, nil
+	if err := validateJSON(jsonServer.Spec.JsonConfig); err != nil {
+		log.Error(err, "Invalid JSON configuration")
+		return r.updateStatus(ctx, jsonServer, "Error", "Error: spec.jsonConfig is not a valid json object")
+	}
+
+	// Set Synced state
+	return r.updateStatus(ctx, jsonServer, "Synced", "Synced succesfully!")
 }
 
 // SetupWithManager sets up the controller with the Manager.
@@ -66,3 +86,29 @@ func (r *JsonServerReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		Named("jsonserver").
 		Complete(r)
 }
+
+// Helper functions
+
+// validateJSON checks if the input string is a valid JSON
+func validateJSON(input string) error {
+	var js json.RawMessage
+	return json.Unmarshal([]byte(input), &js)
+}
+
+// updateStatus updates the status of the JsonServer resource
+func (r *JsonServerReconciler) updateStatus(ctx context.Context, jsonServer *examplev1.JsonServer, state, message string) (ctrl.Result, error) {
+	log := logf.FromContext(ctx)
+
+	// Update status if needed
+	if jsonServer.Status.State != state || jsonServer.Status.Message != message {
+		jsonServer.Status.State = state
+		jsonServer.Status.Message = message
+		if err := r.Status().Update(ctx, jsonServer); err != nil {
+			log.Error(err, "Failed to update JsonServer status")
+			return ctrl.Result{}, err
+		}
+	}
+
+	return ctrl.Result{}, nil
+}
+
